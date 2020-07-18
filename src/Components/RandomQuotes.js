1344e3fa3e3a3f33eebce4995d9aac1714f85b41
Suhas CV <suhazcsk@gmail.com>
2020-07-18 09:13:31 -0400

    init

---
+++ src/Components/RandomQuotes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './RandomQoutes.css';
+
+class RandomQoutes extends React.Component{
+    constructor(props){
+        super(props);
+        this.state=[]
+    }
+
+    async componentDidMount(){
+        let data = await fetch('')
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
