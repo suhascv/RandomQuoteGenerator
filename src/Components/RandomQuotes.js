dd16aa3cd1a1f65dded68362302e4b2a136602a3
Suhas CV <suhazcsk@gmail.com>
2020-07-18 10:21:26 -0400

    genrating random qoutes

--- src/Components/RandomQuotes.js
+++ src/Components/RandomQuotes.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import './RandomQoutes.css';
+import './RandomQuotes.css';
+import data from '../data/data.json';
 
-class RandomQoutes extends React.Component{
+
+class RandomQuotes extends React.Component{
     constructor(props){
         super(props);
-        this.state=[]
+        this.state={
+                current:Math.floor(Math.random()*data.length),
+                bgcolor:0,
+                colors:['#223344','#ff2211','#ee2211','#ddeeaa','#102030','#aa10cc']
+            }
+        this.changeQuote = this.changeQuote.bind(this);
     }
 
-    async componentDidMount(){
-        let data = await fetch('')
+    changeQuote(){
+        let newNum = Math.floor(Math.random()*(data.length-1));
+        this.setState({current:newNum});
+        if(newNum == this.state.current){
+            this.setState({current:newNum+1});
+        }   
+        let newColor = Math.floor(Math.random()*(this.state.colors.length-1))
+        this.setState({bgcolor:this.state.colors[newColor]})
+        if(newColor==this.state.bgcolor){
+            this.setState({bgcolor:newColor+1})
+        }
     }
 
     render(){
         return (
-            <div>
-
+            <div style={{backgroundColor:this.state.colors[this.state.bgcolor]}} className="container">
+                <div className="quote-container">
+                <h1>{data[this.state.current]['text']}</h1>
+        <h4>{data[this.state.current]['author']}</h4>
+                </div>
+        <button onClick={this.changeQuote}>Change Quote</button>
             </div>
         )
     }
 }
+
+
+
+
+export default RandomQuotes;
